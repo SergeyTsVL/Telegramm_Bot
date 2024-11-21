481117dd44772664a50d80cbcfb280a0b089ee4a
tsars <tsarskiytsarskiy@mail.ru>
2024-11-21 11:25:50 +1000

    3.5

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import requests
 import telebot
 from PIL import Image, ImageOps
 import io
@@ -17,27 +18,23 @@ def resize_image(image, new_width=100):
     width, height = image.size
     ratio = height / width
     new_height = int(new_width * ratio)
+    print(image)
     return image.resize((new_width, new_height))
-
 
 def grayify(image):
     return image.convert("L")
 
-
 def image_to_ascii(image_stream, new_width=40):
     # Переводим в оттенки серого
     image = Image.open(image_stream).convert('L')
-
     # меняем размер сохраняя отношение сторон
     width, height = image.size
     aspect_ratio = height / float(width)
     new_height = int(
         aspect_ratio * new_width * 0.55)  # 0,55 так как буквы выше чем шире
     img_resized = image.resize((new_width, new_height))
-
     img_str = pixels_to_ascii(img_resized)
     img_width = img_resized.width
-
     max_characters = 4000 - (new_width + 1)
     max_rows = max_characters // (new_width + 1)
 
@@ -46,7 +43,6 @@ def image_to_ascii(image_stream, new_width=40):
         ascii_art += img_str[i:i + img_width] + "\n"
     return ascii_art
 
-
 def pixels_to_ascii(image):
     pixels = image.getdata()
     characters = ""
@@ -54,8 +50,6 @@ def pixels_to_ascii(image):
         characters += ASCII_CHARS[pixel * len(ASCII_CHARS) // 256]
     return characters
 
-
-# Огрубляем изображение
 def pixelate_image(image, pixel_size):
     image = image.resize(
         (image.size[0] // pixel_size, image.size[1] // pixel_size),
@@ -86,7 +80,8 @@ def get_options_keyboard():
     invert_btn = types.InlineKeyboardButton("Invert colors", callback_data="invert")
     reflected_btn = types.InlineKeyboardButton("Reflected image", callback_data="reflected")
     heatmap_btn = types.InlineKeyboardButton("Convert to heatmap", callback_data="heatmap")
-    keyboard.add(pixelate_btn, ascii_btn, invert_btn, reflected_btn, heatmap_btn)
+    size_btn = types.InlineKeyboardButton("Size", callback_data="change_size")
+    keyboard.add(pixelate_btn, ascii_btn, invert_btn, reflected_btn, heatmap_btn, size_btn)
     return keyboard
 
 @bot.message_handler(content_types=['text'])
@@ -95,7 +90,6 @@ def text_processing(message):
     CHARACTER_SET = str(message.text)
     ASCII_CHARS = CHARACTER_SET or '@%#*+=-:. '
     ascii_and_send(message)
-    # print(message)
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
@@ -114,7 +108,34 @@ def callback_query(call):
     elif call.data == "heatmap":
         bot.answer_callback_query(call.id, "Converting your image heatmap...")
         convert_to_heatmap(call.message)
-        # convert_to_heatmap
+    elif call.data == "change_size":
+        bot.answer_callback_query(call.id, "Converting your size image...")
+        # Проверяем есть ли фото в наличии
+        if call.message.json['reply_to_message']['photo'][-1]['file_id']:  # да... структура зависит от версии пайтона
+            # Получаем информацию о файле
+            file_info = bot.get_file(call.message.json['reply_to_message']['photo'][-1]['file_id'])
+            file_path = f"https://api.telegram.org/file/bot{bot.token}/{file_info.file_path}"
+            try:
+                # Загружаем и изменяем изображение
+                response = requests.get(file_path)
+                response.raise_for_status()
+                image_stream = response.content
+                image = Image.open(io.BytesIO(image_stream))
+                resized_image = resize_for_sticker(image)
+                # Сохраняем измененное изображение в поток
+                buffer = io.BytesIO()
+                resized_image.save(buffer, format='JPEG')
+                buffer.seek(0)
+                # Отправляем измененное изображение обратно в Telegram
+                bot.send_photo(
+                    chat_id=call.message.chat.id,
+                    photo=buffer,
+                    caption="Измененное изображение"
+                )
+            except Exception as e:
+                bot.answer_callback_query(call.id, f"An error occurred: {str(e)}")
+        else:
+            bot.answer_callback_query(call.id, "No photo found in this message.")
 
 def pixelate_and_send(message):
     photo_id = user_states[message.chat.id]['photo']
@@ -194,5 +215,22 @@ def convert_to_heatmap(message):
     output_stream.seek(0)
     bot.send_photo(message.chat.id, output_stream)
     return heatmap_image
+
+def resize_for_sticker(image, max_size=128):
+    """
+    Изменение размера фото до 128
+    """
+    # Получаем текущие размеры изображения
+    width, height = image.size
+    # Определяем новые размеры, сохраняя пропорции
+    if width > height:
+        new_width = max_size
+        new_height = int(height * max_size / width)
+    else:
+        new_height = max_size
+        new_width = int(width * max_size / height)
+    # Создаем новое изображение с заданными размерами
+    resized_image = image.resize((new_width, new_height), Image.LANCZOS)
+    return resized_image
 
 bot.polling(none_stop=True)
--- probe.py
+++ probe.py
@@ -1,8 +1,101 @@
-from PIL import Image, ImageOps
+# from PIL import Image
+#
+# img = Image.open("cats.png")
+#
+# # Задаем новый размер
+# new_size = (800, 600)
+#
+# # Вычисляем коэффициент масштабирования
+# scale_factor = min(new_size[0] / img.size[0], new_size[1] / img.size[1])
+#
+# # Вычисляем новые размеры с сохранением пропорций
+# new_width = int(img.size[0] * scale_factor)
+# new_height = int(img.size[1] * scale_factor)
+#
+# # Изменяем размер изображения
+# img = img.resize((new_width, new_height))
+#
+# # Создаем новое изображение нужного размера и заполняем его белым цветом
+# result_img = Image.new(img.mode, new_size, (255, 255, 255))
+# result_img.paste(img, ((new_size[0] - new_width) // 2, (new_size[1] - new_height) // 2))
+#
+# # Сохраняем результат
+# result_img.save("resized_with_padding.png")
 
-# creating an image object 
-img = Image.open("cats.png").convert("L")
 
-# image colorize function 
-img1 = ImageOps.colorize(img, black="blue", white="white")
-img1.show() 
+#
+# from telebot import TeleBot
+# from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+#
+# # Создаем экземпляр бота
+# bot = TeleBot('YOUR_BOT_TOKEN')
+#
+# # Создаем клавиатуру с кнопками
+# keyboard = InlineKeyboardMarkup()
+# button_yes = InlineKeyboardButton(text='Да', callback_data='yes')
+# button_no = InlineKeyboardButton(text='Нет', callback_data='no')
+# keyboard.add(button_yes, button_no)
+#
+# # Функция для обработки нажатия кнопок
+# def handle_callback(call):
+#     if call.data == 'yes':
+#         bot.answer_callback_query(call.id, text="Вы выбрали Да")
+#         bot.send_message(call.message.chat.id, "Спасибо за ответ!")
+#     elif call.data == 'no':
+#         bot.answer_callback_query(call.id, text="Вы выбрали Нет")
+#         bot.send_message(call.message.chat.id, "Понятно, продолжайте!")
+#
+# # Обработчик сообщений
+# @bot.message_handler(commands=['start'])
+# def start_message(message):
+#     bot.send_message(message.chat.id,
+#                      "Привет! Выберите один из вариантов:",
+#                      reply_markup=keyboard)
+#
+# # Запускаем обработку нажатий кнопок
+# bot.set_my_commands(['/start'])
+#
+# # Запускаем бота
+# bot.polling(none_stop=True)
+
+from telebot import TeleBot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+import os
+import telebot
+
+# Создаем экземпляр бота
+TOKEN = os.environ.get("TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+# Создаем клавиатуру с кнопками
+keyboard = InlineKeyboardMarkup()
+button_large = InlineKeyboardButton(text='Большой', callback_data='large')
+button_medium = InlineKeyboardButton(text='Средний', callback_data='medium')
+button_small = InlineKeyboardButton(text='Маленький', callback_data='small')
+keyboard.add(button_large, button_medium, button_small)
+
+
+# Обработчик нажатия кнопок
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    # Получаем размер стикера из callback_data
+    sticker_size = call.data
+
+    # Отправляем сообщение с результатом выбора
+    bot.answer_callback_query(call.id, text="Вы выбрали размер стикера")
+
+    # Отправляем сообщение с результатом выбора и информацией о размере
+    bot.send_message(call.message.chat.id,
+                     f"Вы выбрали {sticker_size} размер стикера",
+                     reply_markup=types.ReplyKeyboardRemove())
+
+
+# Функция для отправки начального сообщения с клавиатурой
+def send_start_message():
+    bot.send_message(bot.get_updates()[0].message.chat.id,
+                     "Выберите размер стикера:",
+                     reply_markup=keyboard)
+
+
+# Запускаем бота
+bot.polling(none_stop=True)

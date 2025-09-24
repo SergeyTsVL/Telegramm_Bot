bc08e1e4919c651a2abda5ebc5effac4c217722b
tsars <tsarskiytsarskiy@mail.ru>
2025-09-24 19:25:25 +0700

    4

--- bot111.py
+++ bot111.py
@@ -10,9 +10,11 @@ import random
 import requests
 from io import BytesIO
 
-# Прописываю токен в свой пайчарм
+# from probe import bot
 TOKEN = os.environ.get("TOKEN")
 bot = telebot.TeleBot(TOKEN)
+# Прописываю токен в свой пайчарм
+
 
 table_names_list = []
 messages_list = []
@@ -27,12 +29,19 @@ user_states = {}
 user_states_row = {}
 
 list_table = []
+a = 0
+
 
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, f"Посмотрите актуальный список задач ",
                  reply_markup=get_options_keyboard_0())   # добавляем кнопки для вывода и ввода данных
+
+
+
+
+
 def get_options_keyboard_0():
     """
     Добавляем кнопку для вывода случайных шуток из списка файла JOKES.csv
@@ -49,6 +58,7 @@ def get_options_keyboard_0():
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
+
     global list_table
     if call.data == "create_category":
         bot.send_message(call.message.chat.id, 'Введи название новой категории')
@@ -59,113 +69,101 @@ def callback_query(call):
     if call.data == "All_categories":
         list_table = []
 
-        # table_names = print_table_names()
+        table_names = print_table_names()
         if not table_names:
             bot.send_message(call.message.chat.id, "Нет таблиц в базе данных.")
             return
-        keyboard14 = types.InlineKeyboardMarkup()  # Создаем клавиатуру один раз
+        keyboard = types.InlineKeyboardMarkup()  # Создаем клавиатуру один раз
         for table in table_names:
             if table != 'sqlite_sequence':
                 table_names_list.append(table)
-                button123 = types.InlineKeyboardButton(text=table, callback_data=table)  # Создаем кнопку для каждой таблицы
-                keyboard14.add(button123)  # Добавляем кнопку в клавиатуру
-        bot.send_message(call.message.chat.id, "Выберите категорию:", reply_markup=keyboard14)
-        return keyboard14
-
+                button = types.InlineKeyboardButton(text=table, callback_data=table)  # Создаем кнопку для каждой таблицы
+                keyboard.add(button)  # Добавляем кнопку в клавиатуру
+        bot.send_message(call.message.chat.id, "Выберите таблицу:", reply_markup=keyboard)
+        return keyboard
+    global a
     for table in table_names:
-        print(table)
-
         if call.data == table:
             if table != 'sqlite_sequence':
-                list_table.append(table)
+                # print(call.data)
                 # get_all_users(table)
                 users = get_all_users(table, call.message)
-                keyboard1 = types.InlineKeyboardMarkup()  # Создаем клавиатуру один раз
+                # print(users)
+                keyboard = types.InlineKeyboardMarkup()  # Создаем клавиатуру один раз
 
-                button11 = types.InlineKeyboardButton(text='Добавить задание',
-                                                    callback_data='data_work')  # Создаем кнопку для каждой таблицы
+                button = types.InlineKeyboardButton(text='Добавить задание',
+                                                    callback_data='Добавить задание')  # Создаем кнопку для каждой таблицы
 
-                keyboard1.add(button11)  # Добавляем кнопку в клавиатуру
-                bot.send_message(call.message.chat.id, f"Категория '{table}' не имеет заданий ",
-                                 reply_markup=keyboard1)
+                keyboard.add(button)  # Добавляем кнопку в клавиатуру
+                list_table.append(table)
+                bot.send_message(call.message.chat.id, f"Категория '{table}' добавить задание ",
+                                 reply_markup=keyboard)
+
                 if users:
                     keyboard = types.InlineKeyboardMarkup()  # Создаем клавиатуру один раз
-                    # button = types.InlineKeyboardButton(text='Редактирование',
-                    #                                     callback_data='row[2]')  # Создаем кнопку для каждой таблицы
-                    # button1 = types.InlineKeyboardButton(text='Выполнено',
-                    #                                     callback_data='row[0]')  # Создаем кнопку для каждой таблицы
-                    # keyboard.add(button, button1)  # Добавляем кнопку в клавиатуру
-                    d = []
                     for row in users:
-
-                        if 'button' not in d and 'button1' not in d:
-                            # keyboard = types.InlineKeyboardMarkup()  # Создаем клавиатуру один раз
-                            button = types.InlineKeyboardButton(text='Редактирование',
-                                                                callback_data=row[0])  # Создаем кнопку для каждой таблицы
-                            d.append('button')
-                            button1 = types.InlineKeyboardButton(text='Выполнено',
-                                                                 callback_data=row[0])  # Создаем кнопку для каждой таблицы
-                            d.append('button1')
-                            keyboard.add(button, button1)  # Добавляем кнопку в клавиатуру
-                        # d += 1
+                        print(row)
                         if row[4] == 'не_выполнено':
-                            # try:
-                            bot.send_message(call.message.chat.id, f"Категория '{list_table[0]}',\nЗадание: {row[2]},\nДедлайн {row[3]},\nСтатус '{row[4]}'",
-                                             reply_markup=keyboard)
-                            # except:
-                            #     None
+                            button1 = types.InlineKeyboardButton(text='Редактирование',
+                                                                callback_data=row[2])  # Создаем кнопку для каждой таблицы
+
+                            button2 = types.InlineKeyboardButton(text='Выполнено',
+                                                                callback_data=row[0])  # Создаем кнопку для каждой таблицы
+                            keyboard.add(button1, button2)  # Добавляем кнопку в клавиатуру
+
                         else:
-                            # keyboard.add(button)
-                            None
+
+                            a += 1
+                            keyboard.add(button)
+                    bot.send_message(call.message.chat.id, f"Задание: {row[2]},\nдедлайн {row[3]},\nстатус '{row[4]}'",
+                                     reply_markup=keyboard)
 
                     return keyboard
                         # response += f"ID: {row[0]}, Name: {row[1]}"
+
+                    a = 0
                 else:
+                    list_table.append(table)
+
+
+
                     # keyboard = types.InlineKeyboardMarkup()  # Создаем клавиатуру один раз
                     #
                     # button = types.InlineKeyboardButton(text='Добавить задание',
                     #                                     callback_data='Добавить задание')  # Создаем кнопку для каждой таблицы
                     #
                     # keyboard.add(button)  # Добавляем кнопку в клавиатуру
-                    list_table.append(table)
+                    # bot.send_message(call.message.chat.id, f"Категория '{table}' не имеет заданий ",
+                    #                  reply_markup=keyboard)
 
 
-
-    if call.data == "data_work":
+    if call.data == "Добавить задание":
         bot.send_message(call.message.chat.id, 'Введи название новой строки "business"///"created_at"\n'
                                                'через ///\nПример: Разработать микросервис на FastAPI///20.10.25')
         user_states_row[call.message.chat.id] = WAITING_FOR_row
+    print(call.data, 'print(call.data)')
+    try:
+        if [int(call.data)] in range(0, 300):
+            print(call.data, 'call.data')
+            update_row(call.message, call.data)
+    except:
+        None
 
-    if call.data == "row[0]":
-        # update_row(call.data)
-        print(111111)
-    # try:
-    #     if int(call.data) in range(0, 300, 1):
-    #         print(call.data, 'call.data')
-    #         update_row(call.data)
-    #         print(555555)
-    # except:
-    #     None
-
-
-
-
-
-
-
-def update_row(int_call):
-    # print(message, 'message')
-    print(1111)
+def update_row(message, int_call):
+    print(message, 'message')
     conn = sqlite3.connect('example.db')
-    print(2222)
     c = conn.cursor()
-    print(33333)
-    print(list_table)
     c.execute(f'''UPDATE [{list_table[0]}] SET data = ? WHERE id = ?''',
-              ('Выполнено', int(int_call)))
-    print(int_call, 4444444)
+              ('Выполнено', int_call))
+    print(int_call)
     # Сохраняем изменения в базе данных
     conn.commit()
+
+
+
+
+
+
 
 
 def get_all_users(table, message):
@@ -220,42 +218,22 @@ def handle_new_row(message):
     del user_states_row[message.chat.id]
 
 def create_business(message):
-    # bot.send_message(message.chat.id, 'Можно прописывать задание на добавление данных в строку')
-    # print(message['from_user'])
-    # message = json.loads(message)
-    print(message.from_user.username)
-    print(message.from_user.id)
     list_row = message.text.split('///')
-    # print(list_row)
-
-
-
-
     conn = sqlite3.connect('example.db')
     c = conn.cursor()
     c.execute(f'''
     INSERT INTO [{list_table[0]}] (id, username, business, created_at)
     VALUES (?, ?, ?, ?)
-    ''', (message.from_user.id + 1, message.from_user.username, list_row[0], list_row[1]))
+    ''', (message.from_user.id, message.from_user.username, list_row[0], list_row[1]))
     # Сохраняем изменения в базе данных
     conn.commit()
-
-    # 'json': {'message_id': 1737, 'from': {'id': 1105682217, 'is_bot': False, 'first_name': 'Сергей ࣩࣩ ࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩ ࣩࣩ ࣩࣩ ࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩ',
-    # 'username': 'jaga_jagaga', 'language_code': 'ru'}, 'chat': {'id': 1105682217, 'first_name': 'Сергей ࣩࣩ ࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩ ࣩࣩ ࣩࣩ ࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩ',
-    # 'username': 'jaga_jagaga', 'type': 'private'}, 'date': 1758649931, 'text': 'Разработать микросервис на FastAPI///20.10.25'}}
-
-
-
-
-
-
-
 
 
 
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
     bot.reply_to(message, f"Вы написали: {message.text}")
+
     return message.text
 
 def f_create_category(message):
